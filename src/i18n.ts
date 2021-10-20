1ab5c41a50bd05733d65c02648645fe567af0aed
Daniil Prokhvatilov <prokhvatilov@bubuntu-lx.nixsolutions.com>
2021-10-20 17:17:56 +0300

    7: setup localization

---
+++ src/i18n.ts
@@ -0,0 +1,18 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+const resources = {
+  en: {
+    translation: {
+      title: 'React',
+      description: 'JS library',
+    },
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+});
+
+export default i18n;
