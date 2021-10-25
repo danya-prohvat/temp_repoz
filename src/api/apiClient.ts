cccc1316d030917c5ddd82faa9d750840bfe14ed
Daniil Prokhvatilov <prokhvatilov@bubuntu-lx.nixsolutions.com>
2021-10-25 17:11:47 +0300

    11: setup API layer

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,6 +1,10 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { config } from 'config/index';
 
+interface RequestData {
+  user: string;
+}
+
 const instance = axios.create({
   baseURL: config.env.apiBaseUrl,
   // timeout: config.env.axiosDelay,
@@ -13,11 +17,20 @@ export const getRequest = (url: string, config?: AxiosRequestConfig<any> | undef
 export const deleteRequest = (url: string, config?: AxiosRequestConfig<any> | undefined): Promise<any> =>
   instance.delete(url, config);
 
-export const postRequest = (url: string, data?: any, config?: AxiosRequestConfig<any> | undefined): Promise<any> =>
-  instance.post(url, data, config);
+export const postRequest = (
+  url: string,
+  data?: RequestData,
+  config?: AxiosRequestConfig<any> | undefined,
+): Promise<any> => instance.post(url, data, config);
 
-export const putRequest = (url: string, data?: any, config?: AxiosRequestConfig<any> | undefined): Promise<any> =>
-  instance.put(url, data, config);
+export const putRequest = (
+  url: string,
+  data?: RequestData,
+  config?: AxiosRequestConfig<any> | undefined,
+): Promise<any> => instance.put(url, data, config);
 
-export const patchRequest = (url: string, data?: any, config?: AxiosRequestConfig<any> | undefined): Promise<any> =>
-  instance.patch(url, data, config);
+export const patchRequest = (
+  url: string,
+  data?: RequestData,
+  config?: AxiosRequestConfig<any> | undefined,
+): Promise<any> => instance.patch(url, data, config);
