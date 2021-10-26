478c10f767cc5b080252123e74ee26c32253d1af
Dariya Chernyavskaya <dariaachernyavskaya@gmail.com>
2021-10-26 16:44:56 +0300

    15: added icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from '@emotion/react';
 import { Global } from 'styles/Styles';
 import { whiteTheme } from 'styles/themes';
 import { Router } from 'routing/router';
+import 'assets/fonts/icons/style.css';
 
 const App: React.FC = () => {
   return (
--- src/styles/Styles.tsx
+++ src/styles/Styles.tsx
@@ -7,13 +7,27 @@ const Global: React.FC = () => {
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;700&display=swap');
 
+        @font-face {
+          font-family: 'icomoon';
+          src: url('assets/fonts/icons/icomoon.eot');
+          src: url('assets/fonts/icons/icomoon.eot') format('embedded-opentype'),
+            url('assets/fonts/icons/icomoon.ttf') format('truetype'),
+            url('assets/fonts/icons/icomoon.woff') format('woff'), url('assets/fonts/icons/icomoon.svg') format('svg');
+          font-weight: normal;
+          font-style: normal;
+          font-display: block;
+        }
+
+        * {
+          font-family: 'Montserrat';
+        }
+
         *,
         *::before,
         *::after {
           margin: 0;
           padding: 0;
           box-sizing: border-box;
-          font-family: 'Montserrat';
         }
       `}
     />
