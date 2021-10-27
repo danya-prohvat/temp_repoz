ffd72f3f53f0005a7e69e702102293e61082a1a5
Daniil Prokhvatilov <prokhvatilov@bubuntu-lx.nixsolutions.com>
2021-10-27 12:30:42 +0300

    6: setup laoyut

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -23,79 +23,79 @@ const Sidebar: React.FC = () => {
       <RouteLink>
         <Icon type="home" />
         {showSideBar && (
-          <S.Link data-tip data-for="Main">
+          <S.Link data-type="light" data-border={true} data-tip data-for="Main">
             <Typography type="body1">{t('Main')}</Typography>
+            <ReactTooltip id="Main" place="right" effect="solid">
+              {t('Main')}
+            </ReactTooltip>
           </S.Link>
         )}
-        <ReactTooltip id="Main" place="right" effect="solid">
-          {t('Main')}
-        </ReactTooltip>
       </RouteLink>
       <RouteLink>
         <Icon type="compass" />
         {showSideBar && (
-          <S.Link data-tip data-for="Explore">
+          <S.Link data-type="light" data-border={true} data-tip data-for="Explore">
             <Typography type="body1"> {t('Explore')}</Typography>
+            <ReactTooltip id="Explore" place="right" effect="solid">
+              {t('Explore')}
+            </ReactTooltip>
           </S.Link>
         )}
-        <ReactTooltip id="Explore" place="right" effect="solid">
-          {t('Explore')}
-        </ReactTooltip>
       </RouteLink>
       <RouteLink>
         <Icon type="user" />
         {showSideBar && (
-          <S.Link data-tip data-for="Subscribers">
+          <S.Link data-type="light" data-border={true} data-tip data-for="Subscribers">
             <Typography type="body1"> {t('Subscribers')}</Typography>
+            <ReactTooltip id="Subscribers" place="right" effect="solid">
+              {t('Subscribers')}
+            </ReactTooltip>
           </S.Link>
         )}
-        <ReactTooltip id="Subscribers" place="right" effect="solid">
-          {t('Subscribers')}
-        </ReactTooltip>
       </RouteLink>
       <RouteLink>
         <Icon type="user" />
         {showSideBar && (
-          <S.Link data-tip data-for="Subscriptions">
+          <S.Link data-type="light" data-border={true} data-tip data-for="Subscriptions">
             <Typography type="body1"> {t('Subscriptions')}</Typography>
+            <ReactTooltip id="Subscriptions" place="right" effect="solid">
+              {t('Subscriptions')}
+            </ReactTooltip>
           </S.Link>
         )}
-        <ReactTooltip id="Subscriptions" place="right" effect="solid">
-          {t('Subscriptions')}
-        </ReactTooltip>
       </RouteLink>
       <RouteLink>
         <Icon type="message" />
         {showSideBar && (
-          <S.Link data-tip data-for="Messages">
+          <S.Link data-type="light" data-border={true} data-tip data-for="Messages">
             <Typography type="body1"> {t('Messages')}</Typography>
+            <ReactTooltip id="Messages" place="right" effect="solid">
+              {t('Messages')}
+            </ReactTooltip>
           </S.Link>
         )}
-        <ReactTooltip id="Messages" place="right" effect="solid">
-          {t('Messages')}
-        </ReactTooltip>
       </RouteLink>
       <RouteLink>
         <Icon type="saved" />
         {showSideBar && (
-          <S.Link data-tip data-for="Saved">
+          <S.Link data-type="light" data-border={true} data-tip data-for="Saved">
             <Typography type="body1"> {t('Saved')}</Typography>
+            <ReactTooltip id="Saved" place="right" effect="solid">
+              {t('Saved')}
+            </ReactTooltip>
           </S.Link>
         )}
-        <ReactTooltip id="Saved" place="right" effect="solid">
-          {t('Saved')}
-        </ReactTooltip>
       </RouteLink>
       <RouteLink>
         <Icon type="settings" />
         {showSideBar && (
-          <S.Link data-tip data-for="Settings">
+          <S.Link data-type="light" data-border={true} data-tip data-for="Settings">
             <Typography type="body1"> {t('Settings')}</Typography>
+            <ReactTooltip id="Settings" place="right" effect="solid">
+              {t('Settings')}
+            </ReactTooltip>
           </S.Link>
         )}
-        <ReactTooltip id="Settings" place="right" effect="solid">
-          {t('Settings')}
-        </ReactTooltip>
       </RouteLink>
       <S.SidebarButton onClick={toggleSideBarOnClick}>
         <Icon type={showSideBar ? 'arrow-left' : 'arrow-right'} />
